add57e246399b29ffa3f90a66bec3894501f5a30
SamuelGonzalez-2022 <249305486+svm2026@users.noreply.github.com>
2026-02-07 20:56:26 +0100

    cambios src/main

--- src/main/java/com/lauracercas/moviecards/MovieCardsApplication.java
+++ src/main/java/com/lauracercas/moviecards/MovieCardsApplication.java
@@ -2,6 +2,8 @@ package com.lauracercas.moviecards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Autor: Laura Cercas Ramos
@@ -15,4 +17,11 @@ public class MovieCardsApplication {
         SpringApplication.run(MovieCardsApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate template() {
+        RestTemplate template = new RestTemplate();
+        return template;
+    }
+
 }
+
--- src/main/java/com/lauracercas/moviecards/service/actor/ActorServiceImpl.java
+++ src/main/java/com/lauracercas/moviecards/service/actor/ActorServiceImpl.java
@@ -3,8 +3,11 @@ package com.lauracercas.moviecards.service.actor;
 
 import com.lauracercas.moviecards.model.Actor;
 import com.lauracercas.moviecards.repositories.ActorJPA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,24 +18,34 @@ import java.util.List;
 @Service
 public class ActorServiceImpl implements ActorService {
 
-    private final ActorJPA actorJPA;
+    @Autowired
+    RestTemplate template;
 
-    public ActorServiceImpl(ActorJPA actorJPA) {
-        this.actorJPA = actorJPA;
-    }
+    String url = "https://moviecards-service-gonzalez.azurewebsites.net/actors";
 
     @Override
     public List<Actor> getAllActors() {
-        return actorJPA.findAll();
+        Actor[] actores = template.getForObject(url, Actor[].class);
+        List<Actor> actoresList = Arrays.asList(actores);
+        return actoresList;
     }
 
     @Override
     public Actor save(Actor actor) {
-        return actorJPA.save(actor);
+        if (actor.getId() != null && actor.getId() > 0) {
+            template.put(url, actor);
+        } else {
+            actor.setId(0);
+            template.postForObject(url, actor, String.class);
+        }
+        return actor;
     }
 
     @Override
     public Actor getActorById(Integer actorId) {
-        return actorJPA.getById(actorId);
+        Actor actor = template.getForObject(url+"/"+actorId,
+                Actor.class);
+        return actor;
+
     }
 }
