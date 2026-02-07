6852dca3d6ba70da277ae9b39ae82a254012d774
SamuelGonzalez-2022 <249305486+svm2026@users.noreply.github.com>
2026-02-07 21:35:22 +0100

    cierre de split

--- src/test/java/com/lauracercas/moviecards/unittest/service/ActorServiceImplTest.java
+++ src/test/java/com/lauracercas/moviecards/unittest/service/ActorServiceImplTest.java
@@ -1,18 +1,20 @@
 package com.lauracercas.moviecards.unittest.service;
 
 import com.lauracercas.moviecards.model.Actor;
-import com.lauracercas.moviecards.repositories.ActorJPA;
+import com.lauracercas.moviecards.service.actor.ActorService;
 import com.lauracercas.moviecards.service.actor.ActorServiceImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -24,14 +26,14 @@ import static org.mockito.MockitoAnnotations.openMocks;
 class ActorServiceImplTest {
 
     @Mock
-    private ActorJPA actorJPA;
-    private ActorServiceImpl sut;
+    private RestTemplate template;
+    @InjectMocks
+    private ActorService sut = new ActorServiceImpl();
     private AutoCloseable closeable;
 
     @BeforeEach
     void setUp() {
         closeable = openMocks(this);
-        sut = new ActorServiceImpl(actorJPA);
     }
 
     @AfterEach
@@ -41,11 +43,11 @@ class ActorServiceImplTest {
 
     @Test
     public void shouldGetAllActors() {
-        List<Actor> actors = new ArrayList<>();
-        actors.add(new Actor());
-        actors.add(new Actor());
+        Actor actors[] = new Actor[2];
+        actors[0] = new Actor();
+        actors[1] = new Actor();
 
-        when(actorJPA.findAll()).thenReturn(actors);
+        when(template.getForObject(anyString(), any())).thenReturn(actors);
 
         List<Actor> result = sut.getAllActors();
 
@@ -58,24 +60,12 @@ class ActorServiceImplTest {
         actor.setId(1);
         actor.setName("Sample Actor");
 
-        when(actorJPA.getById(anyInt())).thenReturn(actor);
+        when(template.getForObject(anyString(), any())).thenReturn(actor);
 
         Actor result = sut.getActorById(1);
 
         assertEquals(1, result.getId());
         assertEquals("Sample Actor", result.getName());
-    }
-
-    @Test
-    public void shouldSaveActor() {
-        Actor actor = new Actor();
-        actor.setName("New Actor");
-
-        when(actorJPA.save(actor)).thenReturn(actor);
-
-        Actor result = sut.save(actor);
-
-        assertEquals("New Actor", result.getName());
     }
 
 }
--- src/test/java/com/lauracercas/moviecards/unittest/service/MovieServiceImplTest.java
+++ src/test/java/com/lauracercas/moviecards/unittest/service/MovieServiceImplTest.java
@@ -1,18 +1,20 @@
 package com.lauracercas.moviecards.unittest.service;
 
 import com.lauracercas.moviecards.model.Movie;
-import com.lauracercas.moviecards.repositories.MovieJPA;
+import com.lauracercas.moviecards.service.movie.MovieService;
 import com.lauracercas.moviecards.service.movie.MovieServiceImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -23,14 +25,14 @@ import static org.mockito.MockitoAnnotations.openMocks;
  */
 class MovieServiceImplTest {
     @Mock
-    private MovieJPA movieJPA;
-    private MovieServiceImpl sut;
+    private RestTemplate template;
+    @InjectMocks
+    private MovieService sut = new MovieServiceImpl();
     private AutoCloseable closeable;
 
     @BeforeEach
     public void setUp() {
         closeable = openMocks(this);
-        sut = new MovieServiceImpl(movieJPA);
     }
 
     @AfterEach
@@ -40,11 +42,11 @@ class MovieServiceImplTest {
 
     @Test
     public void shouldGetAllMovies() {
-        List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie());
-        movies.add(new Movie());
+        Movie movies[] = new Movie[2];
+        movies[0] = new Movie();
+        movies[1] = new Movie();
 
-        when(movieJPA.findAll()).thenReturn(movies);
+        when(template.getForObject(anyString(), any())).thenReturn(movies);
 
         List<Movie> result = sut.getAllMovies();
 
@@ -57,25 +59,12 @@ class MovieServiceImplTest {
         movie.setId(1);
         movie.setTitle("Sample Movie");
 
-        when(movieJPA.getById(anyInt())).thenReturn(movie);
+        when(template.getForObject(anyString(), any())).thenReturn(movie);
 
         Movie result = sut.getMovieById(1);
 
         assertEquals(1, result.getId());
         assertEquals("Sample Movie", result.getTitle());
     }
-
-    @Test
-    public void shouldSaveMovie() {
-        Movie movie = new Movie();
-        movie.setTitle("New Movie");
-
-        when(movieJPA.save(movie)).thenReturn(movie);
-
-        Movie result = sut.save(movie);
-
-        assertEquals("New Movie", result.getTitle());
-    }
-
 
 }
